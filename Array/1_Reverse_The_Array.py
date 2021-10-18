4a5996508b8a1d6b66bfbd8fe91a10ed66793320
Utkrisht Singh <41757099+utkrisht2000@users.noreply.github.com>
2021-10-18 14:01:04 +0530

    Delete 1_Reverse_The_Array.py

--- Array/1_Reverse_The_Array.py
+++
@@ -1,8 +0,0 @@
-# Reverse the Array
-n = int(input()) # No of Test Case
-
-for i in range(0,n):
-    m = int(input()) # No of element in Array
-    arr = list(map(int,input().split(" "))) # Insertion in array
-    for i in range(0,m):
-        print(arr[(m-1)-i], end = " ") # Reverse the array
